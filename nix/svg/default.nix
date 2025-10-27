9f18d3f0c873ee75a454fffcfc1e7ae16b377bcf
max-ishere <47008271+max-ishere@users.noreply.github.com>
2025-10-27 03:43:11 +0200

    rfct: Remove unused `renderBox` from SVG helpers

--- nix/svg/default.nix
+++ nix/svg/default.nix
@@ -9,9 +9,6 @@
   stylesheet = import ./stylesheet.nix {inherit writeText lib colors;};
 in {
   render = import ./render.nix {inherit runCommand resvg lib stylesheet;};
-  renderBox = import ./render-box.nix {
-    inherit stdenv resvg lib stylesheet;
-  };
   renderIcons = import ./render-icons.nix {
     inherit lib resvg stylesheet stdenv;
   };
